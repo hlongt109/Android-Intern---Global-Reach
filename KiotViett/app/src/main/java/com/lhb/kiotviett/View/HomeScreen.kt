40b01eec4ada4c10786b5a1b0921f764e89aca78
Nguyen Thien Thien <kinaad24@gmail.com>
2024-08-12 13:38:43 +0700

    thien up fullcode

--- KiotViett/app/src/main/java/com/lhb/kiotviett/View/HomeScreen.kt
+++ KiotViett/app/src/main/java/com/lhb/kiotviett/View/HomeScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
@@ -38,6 +39,7 @@ fun HomeScreen() {
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFdddddd))
+            .statusBarsPadding()
     ) {
         Column(
             modifier = Modifier
